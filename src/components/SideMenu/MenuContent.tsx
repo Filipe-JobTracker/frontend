2acdaed11e231cdbaa4e26167fe1ce23c808a730
Filipe Figueiredo <filipefig26@gmail.com>
2024-12-09 15:42:10 +0000

    Made navigation buttons be on top of data insertion buttons

--- src/components/SideMenu/MenuContent.tsx
+++ src/components/SideMenu/MenuContent.tsx
@@ -14,41 +14,45 @@ import RouterButton from "../RouterButton.tsx";
 import NewApplicationModal from "../ApplicationModal/ApplicationModal.tsx";
 import ImportCSV from "../ImportCSV/ImportCSV.tsx";
 import Divider from "@mui/material/Divider";
+import { Button } from '@mui/material';
+import { useThemeContext } from '../../ThemeContext';
+
+const API_DOCS_ENDPOINT = process.env.NODE_ENV === 'production' ? "/docs" : "http://localhost:3000/docs";
 
 const mainListItems = [
     { text: 'Analytics', icon: <AnalyticsRoundedIcon />, url: '/dashboard'},
     { text: 'Applications', icon: <AssignmentIndRoundedIcon />, url: '/applications'},
     { text: 'Companies', icon: <AccountBalanceRoundedIcon /> , url: '/companies'},
 ];
-
 // const newApplicationButton = { text: 'New', color: "success" };
 
 const secondaryListItems = [
-    { text: 'API Docs', icon: <TextSnippetRoundedIcon />, url: "http://localhost:3000/docs" },
-    { text: 'Github', icon: <BugReportRoundedIcon />, url: "https://github.com/filz0r" },
+    { text: 'API Docs', icon: <TextSnippetRoundedIcon />, url: API_DOCS_ENDPOINT },
+    { text: 'Github', icon: <BugReportRoundedIcon />, url: "https://github.com/Filipe-JobTracker/Job-Tracker" },
 ];
 
 export default function MenuContent() {
+    const { toggleTheme } = useThemeContext();
     return (
         <Stack sx={{ flexGrow: 1, p: 1, justifyContent: 'space-between' }}>
             <List dense>
-                <NewApplicationModal />
-                <ImportCSV />
-                <Divider sx={{
-                    mt: 0.75,
-                    mb: 1.25,
-                }}/>
                 {mainListItems.map((item, index) => (
-                    // @ts-ignore
                     <RouterButton key={`menu-item-${index}`} url={item.url} text={item.text} icon={item.icon} sx={{
                         my: 1,
                         width: '100%',
                     }}/>
                 ))}
+                <Divider sx={{
+                    mt: 0.75,
+                    mb: 1.25,
+                }}/>
+                <NewApplicationModal />
+                <ImportCSV />
 
             </List>
 
             <List dense>
+                <Button onClick={toggleTheme}>Toggle Theme</Button>
                 {secondaryListItems.map((item, index) => (
                     <ListItem key={index} disablePadding sx={{ display: 'block' }}>
                         <ListItemButton href={item.url}>
