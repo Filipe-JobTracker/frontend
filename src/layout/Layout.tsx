8001bfe063bb88788e58e80a2bda7c1dbd829db5
filipe <filipefig26@gmail.com>
2025-01-09 16:59:55 +0000

    refactoring frontend but need to move to dev server

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,39 +1,17 @@
 import {Outlet} from 'react-router-dom'
-import SideMenu from "../components/SideMenu/SideMenu.tsx";
 import CssBaseline from "@mui/material/CssBaseline";
-import Box from "@mui/material/Box";
 import Grid from '@mui/material/Grid2';
+import NavBar from "../components/NavBar/NavBar.tsx";
 
 function Layout() {
-    return (
-        <>
+    return (<>
             <CssBaseline enableColorScheme/>
-            <Box sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                justifyContent: 'center',
-                bgcolor: 'background.paper',
-            }}>
-                <Grid container spacing={1} rowSpacing={2}
-                      columnSpacing={{xs: 1, sm: 2, md: 3}}>
-                    <Grid size="auto" sx={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                        flexGrow: 1,
-                    }}>
-                        <SideMenu/>
-                    </Grid>
-                    <Grid display="flex" justifyContent="center"
-                          alignItems="center" size="auto">
-                        <Outlet/>
-                    </Grid>
-                </Grid>
-            </Box>
-        </>
-    )
+            <Grid container spacing={1} rowSpacing={2}
+                  columnSpacing={{xs: 1, sm: 2, md: 3}}>
+                <NavBar/>
+                <Outlet/>
+            </Grid>
+        </>)
 }
 
 export default Layout;
